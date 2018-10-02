654f01ee924cbfc456fcaf27c9ae60bc3b56d10c
Saint-Jean Germila <germila@noukod.com>
2018-10-02 20:01:46 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
    arr = contacts["Freddy Mercury"][:favorite_ice_cream_flavors]
    arr.shift
-   arr
+   return arr
 end
 
       #contacts.each do |person, contact_details_hash|
